f7f599378551db27bf7f0a1be7442022b4ec9bd7
rongwei <rongwei@mail.com>
2019-12-30 23:34:35 +0800

    commit

--- src/com/itheima/test/itheima.java
+++ src/com/itheima/test/itheima.java
@@ -4,7 +4,8 @@ import java.sql.Date;
 
 public class itheima {
 	public void test() {
-		System.out.println("我们都是好孩子");
+		System.out.println("我们都是上面的好孩子");
+		System.out.println("hot_fix...");
 		/*给建议，ctrl+1，根据右边生成左边的数据类型，生成方法
 		 * 删除代码ctrl+d
 		 * 单行注释ctrl+/，多行注释ctrl+shift+/，取消多行注释ctrl+shift+右斜杠
